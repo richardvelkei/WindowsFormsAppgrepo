4d80307c73aca1b23668240a45bc25eace77b602
Richárd Velkei <richard.velkei@stud.uni-corvinus.hu>
2021-11-27 18:50:39 +0100

    Fist step

--- WindowsFormsAppgrepo/Form1.Designer.cs
+++ WindowsFormsAppgrepo/Form1.Designer.cs
@@ -29,13 +29,91 @@ namespace WindowsFormsAppgrepo
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.firstname = new System.Windows.Forms.Label();
+            this.lastname = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(25, 24);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(154, 340);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // firstname
+            // 
+            this.firstname.AutoSize = true;
+            this.firstname.Location = new System.Drawing.Point(195, 32);
+            this.firstname.Name = "firstname";
+            this.firstname.Size = new System.Drawing.Size(70, 17);
+            this.firstname.TabIndex = 1;
+            this.firstname.Text = "Firstname";
+            // 
+            // lastname
+            // 
+            this.lastname.AutoSize = true;
+            this.lastname.Location = new System.Drawing.Point(195, 75);
+            this.lastname.Name = "lastname";
+            this.lastname.Size = new System.Drawing.Size(70, 17);
+            this.lastname.TabIndex = 2;
+            this.lastname.Text = "Lastname";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(271, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(271, 72);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(133, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(198, 117);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(206, 33);
+            this.btnadd.TabIndex = 5;
+            this.btnadd.Text = "button1";
+            this.btnadd.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(428, 384);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lastname);
+            this.Controls.Add(this.firstname);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label firstname;
+        private System.Windows.Forms.Label lastname;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnadd;
     }
 }
 
--- WindowsFormsAppgrepo/Form1.cs
+++ WindowsFormsAppgrepo/Form1.cs
@@ -7,14 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsAppgrepo.Entities;
 
 namespace WindowsFormsAppgrepo
 {
     public partial class Form1 : Form
     {
+        BindigList<User> users = new BindigList<User>();
+
         public Form1()
         {
             InitializeComponent();
+            lastname.Text = Resource1.Lastname;
+            firstname.Text = Resource1.Firstname;
+            btnadd.Text = Resource1.Add;
+
+           
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
