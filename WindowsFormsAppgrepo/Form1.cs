438536cf7646fc0081a6fda220f7b9b7f63b769e
Richárd Velkei <richard.velkei@stud.uni-corvinus.hu>
2021-11-27 20:20:52 +0100

    .

--- WindowsFormsAppgrepo/Form1.cs
+++ WindowsFormsAppgrepo/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,9 +19,10 @@ namespace WindowsFormsAppgrepo
         public Form1()
         {
             InitializeComponent();
-            lastname.Text = Resource1.Lastname;
-            firstname.Text = Resource1.Firstname;
+            fullname.Text = Resource1.Fullname;
             btnadd.Text = Resource1.Add;
+            fajlbairas.Text = Resource1.fajlbairas;
+            delete.Text = Resource1.delete;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -35,9 +37,39 @@ namespace WindowsFormsAppgrepo
         private void btnadd_Click(object sender, EventArgs e)
         {
             User u = new User();
-            u.LastName = lastnamebox.Text;
-            u.FirstName = firstnamebox.Text;
+            u.FullName = txtfullname.Text;
             users.Add(u);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fajlbairas_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog()==DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(sfd.FileName))
+                {
+                    foreach(User item in users)
+                    {
+                        sw.WriteLine(item.ID + ";" + item.FullName);
+                    }
+                }
+            }
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            User valasztottuser = (User)listBox1.SelectedItem;
+            users.Remove(valasztottuser);
         }
     }
 }
