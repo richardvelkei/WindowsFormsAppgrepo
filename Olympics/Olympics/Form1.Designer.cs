c80d70836f2cb05d889f8d6d3a195f10ea633cf2
Richárd Velkei <richard.velkei@stud.uni-corvinus.hu>
2021-12-04 12:00:32 +0100

    Excel kezd

--- Olympics/Olympics/Form1.Designer.cs
+++ Olympics/Olympics/Form1.Designer.cs
@@ -29,20 +29,75 @@ namespace Olympics
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxEv = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnExcel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(22, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Év:";
+            // 
+            // cbxEv
+            // 
+            this.cbxEv.FormattingEnabled = true;
+            this.cbxEv.Location = new System.Drawing.Point(70, 23);
+            this.cbxEv.Name = "cbxEv";
+            this.cbxEv.Size = new System.Drawing.Size(121, 24);
+            this.cbxEv.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 89);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(523, 251);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // btnExcel
+            // 
+            this.btnExcel.Location = new System.Drawing.Point(232, 23);
+            this.btnExcel.Name = "btnExcel";
+            this.btnExcel.Size = new System.Drawing.Size(111, 24);
+            this.btnExcel.TabIndex = 4;
+            this.btnExcel.Text = "Excelbe ment";
+            this.btnExcel.UseVisualStyleBackColor = true;
+            this.btnExcel.Click += new System.EventHandler(this.btnExcel_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(577, 418);
+            this.Controls.Add(this.btnExcel);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cbxEv);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Olmpycs";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxEv;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnExcel;
     }
 }
 
--- Olympics/Olympics/Form1.cs
+++ Olympics/Olympics/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Olympics.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,86 @@ namespace Olympics
 {
     public partial class Form1 : Form
     {
+        List<OlympicsResult> results = new List<OlympicsResult>();
         public Form1()
         {
             InitializeComponent();
+            FileLoader("Summer_olympic_Medals.csv");
+            ComboFeltolt();
+            Osztalyozas();
+            dataGridView1.DataSource = results;
+
+        }
+
+        private void Osztalyozas()
+        {
+            foreach (OlympicsResult item in results)
+            {
+                item.Position = Helyezes(item);
+            }
+        }
+
+        int Helyezes(OlympicsResult res) 
+        {
+            int counter = 0;
+            var szurt = from x in results where x.Year == res.Year && x.Country != res.Country select x;
+            foreach (OlympicsResult item in szurt)
+            {
+                if (item.Medals[0] > res.Medals[0]) counter++;
+                else if ((item.Medals[0] == res.Medals[0]) && (item.Medals[1] > res.Medals[1])) counter++;
+                else if ((item.Medals[0] == res.Medals[0]) && (item.Medals[1] == res.Medals[1]) && (item.Medals[2] > res.Medals[2])) counter++;
+            }
+            return counter + 1;
+        }
+
+        private void ComboFeltolt()
+        {
+            var years = (from x in results orderby x.Year select x.Year).Distinct();
+            cbxEv.DataSource = years.ToList();
+        }
+
+        void FileLoader(string fajlnev)
+        {
+            using (StreamReader sr = new StreamReader(fajlnev))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string sor = sr.ReadLine();
+                    string[] mezok = sor.Split(',');
+                    OlympicsResult or = new OlympicsResult();
+                    or.Year = int.Parse(mezok[0]);
+                    or.Country = mezok[3];
+                    int[] mtomb = new int[3];
+                    mtomb[0] = int.Parse(mezok[5]);
+                    mtomb[1] = int.Parse(mezok[6]);
+                    mtomb[2] = int.Parse(mezok[7]);
+                    or.Medals = mtomb;
+                    results.Add(or);
+
+                }
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btnExcel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
         }
     }
 }
