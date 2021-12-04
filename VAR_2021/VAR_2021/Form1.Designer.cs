ef5e14ee153d69e5f5a67e49eac415e9e59db34d
Richárd Velkei <richard.velkei@stud.uni-corvinus.hu>
2021-12-04 08:54:20 +0100

    Beolvasas

--- VAR_2021/VAR_2021/Form1.Designer.cs
+++ VAR_2021/VAR_2021/Form1.Designer.cs
@@ -29,21 +29,65 @@ namespace VAR_2021
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.saveBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 254);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 312);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(776, 126);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(13, 272);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(163, 34);
+            this.saveBtn.TabIndex = 2;
+            this.saveBtn.Text = "Mentés";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button saveBtn;
     }
 }
 
--- VAR_2021/VAR_2021/Form1.cs
+++ VAR_2021/VAR_2021/Form1.cs
@@ -7,19 +7,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VAR_2021.Entities;
 
 namespace VAR_2021
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> ticks = new List<Tick>(); 
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
         public Form1()
         {
             InitializeComponent();
+            ticks = context.Tick.ToList();
+            dataGridView1.DataSource = ticks;
+
+            CreatePortfolio();
+
+            List<decimal> Nyeresegek = new List<decimal>();
+            int intervalum = 30;
+            DateTime kezdoDatum = (from x in ticks select x.TradingDay).Min();
+            DateTime zaroDatum = new DateTime(2016, 12, 30);
+            TimeSpan z = zaroDatum - kezdoDatum;
+            for (int i = 0; i < z.Days - intervalum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdoDatum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdoDatum.AddDays(i));
+                Nyeresegek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            var nyeresegekRendezve = (from x in Nyeresegek
+                                      orderby x
+                                      select x)
+                                        .ToList();
+            MessageBox.Show(nyeresegekRendezve[nyeresegekRendezve.Count() / 5].ToString());
+        }
+
+        private void CreatePortfolio()
+        {
+            PortfolioItem p = new PortfolioItem();
+            p.Index = "OTP";
+            p.Volume = 10;
+            Portfolio.Add(p);
+
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
+        }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+
+        
     }
 }
