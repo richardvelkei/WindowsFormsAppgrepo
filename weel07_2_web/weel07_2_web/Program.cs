a8ac3233b1e1f131aa641ac337229440072147fc
Richárd Velkei <richard.velkei@stud.uni-corvinus.hu>
2021-12-12 19:04:05 +0100

    Initial commit

---
+++ weel07_2_web/weel07_2_web/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace weel07_2_web
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
