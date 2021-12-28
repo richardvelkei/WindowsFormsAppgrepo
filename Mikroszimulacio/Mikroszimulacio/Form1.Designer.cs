7c619553b9fad9d1b04ac6424d58107eab0df752
Richárd Velkei <richard.velkei@stud.uni-corvinus.hu>
2021-12-28 13:06:25 +0100

    Initial commit

---
+++ Mikroszimulacio/Mikroszimulacio/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace Mikroszimulacio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button browseBtn;
+            this.zaroev = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nepessegBox = new System.Windows.Forms.TextBox();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            browseBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // zaroev
+            // 
+            this.zaroev.Location = new System.Drawing.Point(12, 9);
+            this.zaroev.Name = "zaroev";
+            this.zaroev.Size = new System.Drawing.Size(100, 23);
+            this.zaroev.TabIndex = 0;
+            this.zaroev.Text = "Záróév";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(64, 8);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(271, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Népesség fájl";
+            // 
+            // nepessegBox
+            // 
+            this.nepessegBox.Location = new System.Drawing.Point(371, 7);
+            this.nepessegBox.Name = "nepessegBox";
+            this.nepessegBox.Size = new System.Drawing.Size(255, 22);
+            this.nepessegBox.TabIndex = 3;
+            // 
+            // browseBtn
+            // 
+            browseBtn.Location = new System.Drawing.Point(632, 6);
+            browseBtn.Name = "browseBtn";
+            browseBtn.Size = new System.Drawing.Size(75, 23);
+            browseBtn.TabIndex = 4;
+            browseBtn.Text = "Browse";
+            browseBtn.UseVisualStyleBackColor = true;
+            browseBtn.Click += new System.EventHandler(this.browseBtn_Click);
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(713, 6);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 23);
+            this.startBtn.TabIndex = 5;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 35);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 403);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(browseBtn);
+            this.Controls.Add(this.nepessegBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.zaroev);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label zaroev;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nepessegBox;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Mikroszimulacio/Mikroszimulacio/Form1.cs
@@ -0,0 +1,131 @@
+﻿using Mikroszimulacio.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mikroszimulacio
+{
+    public partial class Form1 : Form
+    {
+        List<Person> Population;
+        List<BirthProbabilities> BirthProbabilities;
+        List<DeathProbabilities> DeathProbabilities;
+
+        Random rng = new Random(1234);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Population = GetPopulation(@"C:\Users\richa\source\repos\WindowsFormsAppgrepo\Mikroszimulacio\Mikroszimulacio\Files\nép-teszt.csv");
+            BirthProbabilities = GetBirthProbabilities(@"C:\Users\richa\source\repos\WindowsFormsAppgrepo\Mikroszimulacio\Mikroszimulacio\Files\születés.csv");
+            DeathProbabilities = GetDeathProbabilities(@"C:\Users\richa\source\repos\WindowsFormsAppgrepo\Mikroszimulacio\Mikroszimulacio\Files\halál.csv");
+
+            Simulation();
+
+        }
+
+        private void Simulation()
+        {
+            for (int year = 2005; year < 2024; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    SzimulacioLepes(Population[i], year);
+                }
+
+                int ferfiakszama = (from x in Population where x.Gender == Gender.Male select x).Count();
+                int nokszama = (from x in Population where x.Gender == Gender.Female select x).Count();
+
+                Console.WriteLine(string.Format("Év: {0} Férfiak: {1} Nők: {2}", year, ferfiakszama, nokszama));
+
+            }
+        }
+
+        private void SzimulacioLepes(Person person, int year)
+        {
+            if (!person.IsAlive) return;
+
+            int kor = year - person.BirthYear;
+
+            double halalvaloszinuseg = (from x in DeathProbabilities where x.Gender == person.Gender && x.Age == year select x.P).FirstOrDefault();
+            double veletlen = rng.NextDouble();
+
+            if (veletlen <= halalvaloszinuseg)
+                person.IsAlive = false;
+
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                double szuletesvalszinuseg = (from x in BirthProbabilities where x.Age == year select x.P).FirstOrDefault();
+                veletlen = rng.NextDouble();
+                if (veletlen <= szuletesvalszinuseg)
+                {
+                    Person baba = new Person();
+                    baba.BirthYear = year;
+                    baba.NbrOfChildren = 0;
+                    baba.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(baba);
+                }
+            }
+        }
+
+        public List<BirthProbabilities> GetBirthProbabilities(string csvpath)
+        {
+            List<BirthProbabilities> result = new List<BirthProbabilities>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    String[] items = line.Split(';');
+                    Person p = new Person();
+                    p.BirthYear = int.Parse(items[0]);
+                    p.Gender = (Gender)Enum.Parse(typeof(Gender), items[1]);
+                    p.NbrOfChildren = int.Parse(items[2]);
+                    result.Add(p);
+                }
+            }
+
+            return result;
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            Simulation((int)numericUpDown1.Value, nepessegBox.Text);
+        }
+
+        private void browseBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK) richTextBox1.Text = ofd.FileName;
+        }
+
+        /*public List<BirthProbabilities> GetBirthProbabilities(string csvpath)
+        {
+            List<BirthProbabilities> result = new List<BirthProbabilities>();
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    String[] items = line.Split(';');
+                    BirthProbabilities p = new BirthProbabilities();
+                    p.Age = int.Parse(items[0]);
+                    p.NbrOfChildren = int.Parse(items[1]);
+                    p.P = int.Parse(items[2]);
+                    result.Add(p);
+                }
+            }
+        
+        }*/
+
+    }
+}
