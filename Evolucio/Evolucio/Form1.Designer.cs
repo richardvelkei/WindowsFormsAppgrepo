00038b626a53a3b5518ce7433f553ef39925879e
Richárd Velkei <richard.velkei@stud.uni-corvinus.hu>
2021-12-28 13:51:39 +0100

    kesz

--- Evolucio/Evolucio/Form1.Designer.cs
+++ Evolucio/Evolucio/Form1.Designer.cs
@@ -29,13 +29,48 @@ namespace Evolucio
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.generationLabel = new System.Windows.Forms.Label();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // generationLabel
+            // 
+            this.generationLabel.AutoSize = true;
+            this.generationLabel.Location = new System.Drawing.Point(12, 370);
+            this.generationLabel.Name = "generationLabel";
+            this.generationLabel.Size = new System.Drawing.Size(87, 17);
+            this.generationLabel.TabIndex = 0;
+            this.generationLabel.Text = "1. generáció";
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(331, 370);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 23);
+            this.startBtn.TabIndex = 1;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Visible = false;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.generationLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label generationLabel;
+        private System.Windows.Forms.Button startBtn;
     }
 }
 
--- Evolucio/Evolucio/Form1.cs
+++ Evolucio/Evolucio/Form1.cs
@@ -7,14 +7,90 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace Evolucio
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+
+        //Induló populáció
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
+        // Gyozteskezeles
+
+        Brain winnerBrain;
+
         public Form1()
         {
             InitializeComponent();
+
+            gc.GameOver += Gc_GameOver;
+
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
+            
+            //gc.AddPlayer();
+            //gc.Start(true);
+
+
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            generationLabel.Text = string.Format("{0}. generáció", generation);
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topplayers = playerList.Take(populationSize / 2).ToList();
+
+            gc.ResetCurrentLevel();
+            foreach (Player p in topplayers)
+            {
+                Brain b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
+
+            var winners = from p in topplayers where p.IsWinner select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                startBtn.Visible = true;
+                return;
+            }
+
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+            ga.Focus();
+            gc.Start(true);
         }
     }
 }
